bffd9571a03e647d6428b28e6164a1d320bae32f
Shaun McKinnon <shaun@firesideagency.ca>
2018-09-23 13:41:30 -0400

    Basic example complete

--- Week-04/Conditions-Loops/conditionsAndLoops.js
+++ Week-04/Conditions-Loops/conditionsAndLoops.js
@@ -2,26 +2,41 @@ const message = document.querySelector('#message');
 const productsEle = document.querySelector('#products');
 
 
-// let name = prompt("What is your name?");
-// if (name && name.length > 0) {
-//   message.textContent = `Hello, ${name}.`;
-// }
+let name = prompt("What is your name?");
+if (name && name.length > 0) {
+  message.textContent = `Hello, ${name}.`;
+}
 
 
-// let confirmation = confirm("Are you here to shop?");
-// if (confirmation === true) {
-//   message.textContent += "\nWelcome to our shop, Big spender!!!";
-// } else {
-//   message.textContent += "\nCool beans, cheapskate!";
-// }
+let confirmation = confirm("Are you here to shop?");
+if (confirmation === true) {
+  message.textContent += "\nWelcome to our shop, Big spender!!!";
+} else {
+  message.textContent += "\nCool beans, cheapskate!";
+}
 
 
 const priceRange = document.querySelector('#priceRange');
 const priceRangeValue = document.querySelector('#priceRangeValue');
 priceRangeValue.textContent = priceRange.value;
 priceRange.oninput = function () {
-  priceRange.value = priceRange.value > 0 ? priceRange.value : 20;
+  priceRange.value = priceRange.value > 0 ? priceRange.value : 10;
   priceRangeValue.textContent = priceRange.value;
+
+  // clear the product display property
+  outputProducts(priceRange.value);
+}
+
+
+const searchInput = document.querySelector('#searchInput');
+searchInput.oninput = function (event) {
+  let searchValue = event.target.value
+
+  if (searchValue.length >= 3 || searchValue === "") {
+    let price = priceRangeValue.textContent;
+
+    outputProducts(price, searchValue);
+  }
 }
 
 
@@ -39,19 +54,31 @@ for (let i = 0; i < navItems.length; i++) {
   nav.appendChild(navItem);
 }
 
+function outputProducts (price = null, search = "") {
+  productsEle.innerHTML = "";
 
-for (let category in products) {
-  for (let product of products[category]) {
-    let productTemplate = document.querySelector('#productTemplate').content.cloneNode(true);
-    let img = productTemplate.querySelector('img');
-    let name = productTemplate.querySelector('h4');
-    let price = productTemplate.querySelector('p');
+  for (let category in products) {
+    for (let product of products[category]) {
+      if (price === null || product.price > price) {
+        let searchTerm = new RegExp(search, 'g');
+        if (search === "" || product.name.toLowerCase().match(searchTerm)) {
+          let productTemplate = document.querySelector('#productTemplate').content.cloneNode(true);
+          let img = productTemplate.querySelector('img');
+          let name = productTemplate.querySelector('h4');
+          let price = productTemplate.querySelector('p');
 
-    if (product.src) img.src = product.src;
-    if (product.name) img.alt = product.name;
-    if(product.name) name.textContent = product.name;
-    if(product.price) price.textContent = `$${product.price}`;
+          if (product.src) img.src = product.src;
+          if (product.name) img.alt = product.name;
+          if(product.name) name.textContent = product.name;
+          if(product.price) price.textContent = `$${product.price}`;
 
-    productsEle.appendChild(productTemplate);
+          productsEle.appendChild(productTemplate);
+        }
+      } else {
+        continue;
+      }
+    }
   }
 }
+
+outputProducts();
