7cdb778c83c1ac09d98ead8f7768d576701b34ee
Shaun McKinnon <shaun.mckinnon@mintent.com>
2018-11-24 13:01:52 -0500

    worky work work work

--- Week-12/main.js
+++ Week-12/main.js
@@ -1,6 +1,22 @@
-const requestURL = "https://gcsmckinnon.netlify.com/week-12/data.json";
+// const requestURL = "https://gcsmckinnon.netlify.com/week-12/data.json";
+const requestURL = "https://insult.mattbas.org/api/insult.json";
+const headers = new Headers();
 
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+fetch(requestURL, {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Request-Method": "GET",
+    "Origin": "https://gcsmckinnon.netlify.com"
+  },
+  method: 'GET',
+  cache: 'no-cache',
+  credentials: 'include',
+  mode: 'cors'
+})
+.then(function (response) {
+  console.log(response);
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
